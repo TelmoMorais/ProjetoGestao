85e3c055fc673157f05349b36691436d8e103923
Leonardo Marques <leonardo2000marques@gmail.com>
2021-11-12 11:53:51 +0000

    Criação da classe para novo gestor.

---
+++ Models/NovoGestor.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ProjetoGestao.Models
+{
+    public class NovoGestor
+    {
+        public int NovoGestorId { get; set; }
+
+        [Required]
+        [StringLength(512)]
+        public string Title { get; set; }
+
+        public string Description { get; set; }
+    }
+}
