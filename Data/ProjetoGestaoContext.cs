7fab6d3236bc5fc5bf19ad98fcb8f982ca5a2563
Leonardo Marques <leonardo2000marques@gmail.com>
2021-11-12 12:26:29 +0000

    Novo gestor scaffolding

--- Data/ProjetoGestaoContext.cs
+++ Data/ProjetoGestaoContext.cs
@@ -15,5 +15,7 @@ namespace ProjetoGestao.Data
         }
 
         public DbSet<ProjetoGestao.Models.NovoProjeto> NovoProjeto { get; set; }
+
+        public DbSet<ProjetoGestao.Models.NovoGestor> NovoGestor { get; set; }
     }
 }
