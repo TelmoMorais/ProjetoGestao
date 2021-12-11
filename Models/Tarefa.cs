b0f12f0584157da9cc51b8655acc0f31a2f11b6e
Leonardo Marques <leonardo2000marques@gmail.com>
2021-12-11 11:28:27 +0000

    Criacao da tabela de tarefas

--- Models/Tarefa.cs
+++ Models/Tarefa.cs
@@ -6,14 +6,13 @@ using System.Threading.Tasks;
 
 namespace ProjetoGestao.Models
 {
-    public class Projeto
+    public class Tarefa
     {
-        public int ProjetoId { get; set; }
+        public int TarefaId { get; set; }
 
         [Required]
         [StringLength(512)]
-        public string NomeProjeto { get; set; }
-        public string DescricaoProjeto { get; set; }
+        public string NomeTarefa { get; set; }
 
         [Required]
         public string DataPrevistaInicio { get; set; }
@@ -27,14 +26,10 @@ namespace ProjetoGestao.Models
         [Required]
         public string DataPrevistaFim { get; set; }
 
+        public string ColaboradorResponsavelTarefa { get; set; }
+        public string EstadoTarefa { get; set; }
 
-        public int GestorId { get; set; }
-        public Gestor Gestor { get; set; }
-
-        public int ClienteId { get; set; }
-        public Cliente Cliente { get; set; }
-
-        public ICollection<ColaboradoresProjeto> ColaboradoresProjetos { get; set; }
-        public ICollection<Tarefa> Tarefas { get; set; }
+        public int ProjetoId { get; set; }
+        public Projeto Projeto { get; set; }
     }
 }
