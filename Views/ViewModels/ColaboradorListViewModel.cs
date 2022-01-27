c87f28c8c1efc89041fe83ba4606deabc0c61909
Fabio Abreu <fabiofiqueli@hotmail.com>
2022-01-27 16:33:48 +0000

    Add ColaboradorListViewModel

---
+++ Views/ViewModels/ColaboradorListViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ProjetoGestao.Models;
+
+namespace ProjetoGestao.Views.ViewModels
+{
+    public class ColaboradorListViewModel
+    {
+        public IEnumerable<Colaborador> Colaboradors { get; set; }
+        public PagingInfo PagingInfo { get; set; }
+        public string ProcuraNome { get; set; }
+    }
+}
