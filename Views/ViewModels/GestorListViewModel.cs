03e410f1ca5adf38448db82477b8c5792e225fab
Fabio Abreu <fabiofiqueli@hotmail.com>
2022-01-27 16:47:40 +0000

    Add GestorListViewModel

---
+++ Views/ViewModels/GestorListViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ProjetoGestao.Models;
+
+namespace ProjetoGestao.Views.ViewModels
+{
+    public class GestorListViewModel
+    {
+        public IEnumerable<Gestor> Gestors { get; set; }
+        public PagingInfo PagingInfo { get; set; }
+        public string ProcuraNome { get; set; }
+
+    }
+}
