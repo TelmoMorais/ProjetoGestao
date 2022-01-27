c0645588de49473c88767a90f25e22085e888129
Fabio Abreu <fabiofiqueli@hotmail.com>
2022-01-27 16:41:47 +0000

    Add TarefaListViewModel

---
+++ Views/ViewModels/TarefaListViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ProjetoGestao.Models;
+
+namespace ProjetoGestao.Views.ViewModels
+{
+    public class TarefaListViewModel
+    {
+        public IEnumerable<Tarefa> Tarefas { get; set; }
+        public PagingInfo PagingInfo { get; set; }
+        public string ProcuraNome { get; set; }
+    }
+}
