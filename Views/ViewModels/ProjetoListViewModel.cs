be6fcefc6bc16c8c2b96bbb219e0b99ebfa53c4c
Fabio Abreu <fabiofiqueli@hotmail.com>
2022-01-27 16:38:56 +0000

    Add ProjetoListViewModel

---
+++ Views/ViewModels/ProjetoListViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ProjetoGestao.Models;
+
+namespace ProjetoGestao.Views.ViewModels
+{
+    public class ProjetoListViewModel
+    {
+        public IEnumerable<Projeto> Projetos { get; set; }
+        public PagingInfo PagingInfo { get; set; }
+        public string ProcuraNome { get; set; }
+    }
+}
